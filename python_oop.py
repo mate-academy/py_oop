6f2aadcc31d04bf84014de5b9973a5efb7bb6395
gooddmas <up2dmas@gmail.com>
2020-07-20 14:57:21 +0300

    python_oop.py done

--- python_oop.py
+++ python_oop.py
@@ -14,9 +14,83 @@ Testing:
 
 
 class TestRunBuilder:
-    pass
 
-# test_run_smoke = TestRunBuilder(username, user_email, user_password)
-# test_run_sanity = TestRunBuilder(username, user_email, user_password)
-# test_run_regression = TestRunBuilder(username, user_email, user_password)
+    registration_url = 'https://react-redux.realworld.io/#/register'
+    login_url = 'https://react-redux.realworld.io/#/login'
+    article_creation_url = 'https://react-redux.realworld.io/#/editor'
+    article_test_data = 'Default'
 
+    # Main constructor
+    def __init__(self, first_name, last_name, user_email, user_password):
+        username_list = [first_name, last_name]
+        self.username = ''.join(username_list)
+        splits = len(user_email.split('@'))
+        if (splits == 2) and ("@" in user_email):
+            self.user_email = user_email
+        else:
+            print('Email field is not valid')
+        self.user_password = user_password
+        # comment line under this if you want to see the password in console
+        self.user_password = '********'
+
+    # Login flow
+    def login_flow(self):
+        print(f'- Open {self.login_url} page')
+        print(f'- Fill Username field as {self.user_email}')
+        print(f'- Fill Password field as {self.user_password}')
+        print('- Click on [Sign in] button')
+
+    # Test suite for Registration page
+    def registration_test_suite(self):
+        print('-----------------------------')
+        print('::: CHECKING REGISTRATION PAGE :::')
+        print(f'- Open {self.registration_url} page')
+        print(f'- Fill Username field as {self.username}')
+        print(f'- Fill Email field as {self.user_email}')
+        print(f'- Fill Password field as {self.user_password}')
+        print('- Click on [Sign in] button')
+        print('- Check the redirection to main page is working after successful registration')
+
+    # Test suite for Login page
+    def login_test_suite(self):
+        print('-----------------------------')
+        print('::: CHECKING LOGIN PAGE :::')
+        self.login_flow()
+        print('- Check the redirection to main page is working after successful login')
+
+    # Test suite for Create Article page
+    def publish_article_test_suite(self):
+        print('-----------------------------')
+        print('::: CHECKING CREATE ARTICLE PAGE :::')
+        self.login_flow()
+        print(f'- Open {self.article_creation_url} page')
+        print(f'- Fill all fields with "{self.article_test_data}" text')
+        print('- Click on [Publish Article] button')
+        print('- Check if redirection to published page is working')
+
+    # Smoke Test Run
+    def smoke_test_run(self):
+        print('-----------------------------')
+        print('You are running smoke flow')
+        self.registration_test_suite()
+        self.login_test_suite()
+
+    # Sanity Test Run
+    def sanity_test_run(self):
+        print('-----------------------------')
+        print('You are running sanity flow')
+        self.publish_article_test_suite()
+
+    # Regression Test Run
+    def regression_test_run(self):
+        print('-----------------------------')
+        print('You are running regression flow')
+        self.registration_test_suite()
+        self.login_test_suite()
+        self.publish_article_test_suite()
+
+test_data = TestRunBuilder('Dmitriy', 'Lebed', 'up2dmas+12345@gmail.com', '12345678')
+
+test_data.smoke_test_run()
+test_data.sanity_test_run()
+test_data.regression_test_run()
